0e9296ead78624a729ba407d1f48be00a542b429
junjie <junjie09260550@163.com>
2021-05-21 17:17:03 +0800

    员工相关service实现类

---
+++ hr-admin-system/hr-admin-system-service/src/main/java/com/hr/system/service/impl/PositionServiceImpl.java
@@ -0,0 +1,20 @@
+package com.hr.system.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hr.system.entity.Position;
+import com.hr.system.mapper.PositionMapper;
+import com.hr.system.service.PositionService;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author junjie
+ * @since 2021-05-21
+ */
+@Service
+public class PositionServiceImpl extends ServiceImpl<PositionMapper, Position> implements PositionService {
+
+}
