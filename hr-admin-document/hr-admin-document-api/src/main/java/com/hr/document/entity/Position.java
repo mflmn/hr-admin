f57eb4dc2a156fcca18b1ce480e82fca0cd6c984
junjie <junjie09260550@163.com>
2021-05-21 19:07:03 +0800

    员工相关实体类

---
+++ hr-admin-document/hr-admin-document-api/src/main/java/com/hr/document/entity/Position.java
@@ -0,0 +1,49 @@
+package com.hr.document.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author junjie
+ * @since 2021-05-21
+ */
+@Data
+  @EqualsAndHashCode(callSuper = false)
+    public class Position implements Serializable {
+
+    private static final long serialVersionUID=1L;
+
+      /**
+     * id
+     */
+        @TableId(value = "id", type = IdType.AUTO)
+      private Integer id;
+
+      /**
+     * 职位
+     */
+      private String name;
+
+      /**
+     * 创建时间
+     */
+      @TableField("createDate")
+    private LocalDateTime createDate;
+
+      /**
+     * 是否启用
+     */
+      private Boolean enabled;
+
+
+}
