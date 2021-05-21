311bf76f93f3a7ed2009e54f3da1689018076225
junjie <junjie09260550@163.com>
2021-05-21 17:08:28 +0800

    员工相关mapper及xml

---
+++ hr-admin-system/hr-admin-system-service/src/main/java/com/hr/system/mapper/EmployeeMapper.java
@@ -0,0 +1,18 @@
+package com.hr.system.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hr.system.entity.Employee;
+import org.springframework.stereotype.Repository;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author admin
+ * @since 2021-05-21
+ */
+@Repository
+public interface EmployeeMapper extends BaseMapper<Employee> {
+
+}
---
+++ hr-admin-system/hr-admin-system-service/src/main/java/com/hr/system/mapper/NationMapper.java
@@ -0,0 +1,18 @@
+package com.hr.system.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hr.system.entity.Nation;
+import org.springframework.stereotype.Repository;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author admin
+ * @since 2021-05-21
+ */
+@Repository
+public interface NationMapper extends BaseMapper<Nation> {
+
+}
