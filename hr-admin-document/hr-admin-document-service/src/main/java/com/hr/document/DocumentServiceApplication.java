01e570253d979321e35165b704972137e9a50189
junjie <junjie09260550@163.com>
2021-05-22 12:51:35 +0800

    document service start

---
+++ hr-admin-document/hr-admin-document-service/src/main/java/com/hr/document/DocumentServiceApplication.java
@@ -0,0 +1,15 @@
+package com.hr.document;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author junjie
+ * @version 1.0.0
+ */
+@SpringBootApplication
+public class DocumentServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentServiceApplication.class,args);
+    }
+}
